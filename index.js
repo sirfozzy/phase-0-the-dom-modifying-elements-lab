4a7f51ca1d281d9119df4dc883ee677442fb14a6
Jason Antman <antmanjt@gmail.com>
2022-03-03 16:39:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//1
+document.querySelector('main').remove();
+
+//2
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//3
+
+newHeader.setAttribute("h1", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
